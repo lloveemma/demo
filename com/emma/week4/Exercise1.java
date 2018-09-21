b46a03efcce5864544362de9d47bbfb05ced4492
lloveemma <43458629+lloveemma@users.noreply.github.com>
2018-09-20 22:07:36 -0500

    Add files via upload

---
+++ com/emma/week4/Exercise1.java
@@ -0,0 +1,36 @@
+package com.emma.week4;
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        new Exercise1().run();
+    }
+
+    public void run() {
+        // Your code goes below this line.
+
+        // You can print the board like you did earlier.. But don't forget the variables if you do!
+
+        // Step 1 Prompt for input
+    	System.out.print("Please select a field (1-9): ");
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		int step = scanner.nextInt();
+		boolean exit = false;
+        // Step 2 Verify this.. "as long as the input is not a number between 1 - 9" - > repeat the following
+		do {
+			if (step < 1 || step > 9) {
+				// Step 2b: Ask for new input
+				System.out.println("This value is out of range. Please enter a value between 1 and 9.");
+				System.out.print("Please select a field (1-9): ");
+				step = scanner.nextInt();
+			}else{
+				exit = true;
+			}
+		} while (!exit);
+		
+        // Step 3 Send out confirmation!
+		System.out.println("That is a valid entry!");
+		scanner.close();
+        // End of code.
+    }
+
+}
---
+++ com/emma/week4/Exercise2.java
@@ -0,0 +1,39 @@
+package com.emma.week4;
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        new Exercise2().run();
+    }
+
+    public void run() {
+        // Your code goes below this line.
+    	
+    	java.util.Scanner scanner = new java.util.Scanner(System.in);
+        // Step 1: Prompt for X coordinate
+    	System.out.print("What's the X coordinate? ");
+		int x = scanner.nextInt();
+        // Step 2: Prompt for Y coordinate
+		System.out.print("What's the Y coordinate? ");
+		int y = scanner.nextInt();
+        // Step 2.5: (Optional) Print some borders.. <-- do this last!
+		System.out.println("============");
+        // Step 3: Create a loop for all vertical lines, starting at 0 till Y
+		for (int i = 0; i < 5; i++) {
+			System.out.print("+");
+			// Step 4: Inside this loop, create another loop that starts at 0 till X
+			for (int j = 0; j < 10; j++) {
+				// Step 5: If the inputted X and Y coordinates match where you currently are, print an X.
+				if (x == j && y == i) {
+					System.out.print("X");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println("+");
+		}
+		System.out.println("+==========+");
+		scanner.close();
+        // End of code.
+    }
+
+}
