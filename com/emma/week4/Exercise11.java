7d15be6a3a13a8f48fd7e4540ccaf5c3eb684320
lloveemma <43458629+lloveemma@users.noreply.github.com>
2018-09-24 14:18:13 -0500

    Add files via upload

--- com/emma/week4/Exercise11.java
+++ com/emma/week4/Exercise11.java
@@ -9,8 +9,26 @@ public class Exercise11 {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + "x" + i + "=" + i * j + "\t");// \t为制表符（相当于按Tab键）
             }
-            //每行一个换行
+            // 每行一个换行
             System.out.println();
         }
+
+        System.out.println("Welcome to the multiplication table trainer!");
+        System.out.println();
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Please enter the first table that you want to train: ");
+        int first = scanner.nextInt();
+        System.out.print("Please enter the last table that you want to train: ");
+        System.out.println();
+        int last = scanner.nextInt();
+        scanner.close();
+        for (int i = first; i <= last; i++) {
+            System.out.println("== Table of " + i + " == ");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(j + "x" + i + "=" + i * j);
+            }
+            System.out.println();
+        }
+
     }
 }
