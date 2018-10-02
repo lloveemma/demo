75e59756ad51a6a4c4c8f4629bed7ac0b74c0d5a
lloveemma <43458629+lloveemma@users.noreply.github.com>
2018-10-01 19:55:08 -0500

    Add files via upload

---
+++ com/emma/week5/Exercise2_v2.java
@@ -0,0 +1,31 @@
+package com.emma.week5;
+
+public class Exercise2_v2 {
+
+    public static void main(String[] args) {
+        new Exercise2_v2().run();
+    }
+
+    public void run() {
+        // Your code goes below this line.
+
+        // Step 1: Prompt the user the length of the array
+        System.out.print("Enter the number of players:  ");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int count = scanner.nextInt();
+        // Step 2: Create an array with the length obtained in step 1
+        String[] players = new String[count];
+        for (int i = 0; i < players.length; i++) {
+            System.out.print("Enter the name of player " + (i + 1) + ":");
+            String name = scanner.next();
+            players[i] = name;
+        }
+        scanner.close();
+        // Step 3: Generate a random number between 0 and array.length
+        int num = new java.util.Random().nextInt(players.length);
+        // Step 4: Print out the value associated with the index that was just generated
+        System.out.println(players[num]+" must go first!");
+        // End of code.
+    }
+
+}
